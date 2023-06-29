6e5791a68883b5bc7b5739e9d2e569f3a55acb05
Surraiya <104488515+Surraiya@users.noreply.github.com>
2023-06-29 11:57:08 +0300

    Update FooterForm.java

--- src/main/java/PageObjects/Forms/MainPageForms/FooterForm.java
+++ src/main/java/PageObjects/Forms/MainPageForms/FooterForm.java
@@ -1,19 +1,1 @@
-package PageObjects.Forms.MainPageForms;
 
-import aquality.selenium.elements.interfaces.ITextBox;
-import aquality.selenium.forms.Form;
-import org.openqa.selenium.By;
-
-
-public class FooterForm extends Form {
-
-    private final ITextBox footerText =  getElementFactory().getTextBox(By.xpath("//div[@class='container']/p/span"), "Footer text");
-
-    public FooterForm(){
-        super(By.xpath("//footer[@class='footer']"), "Footer Form");
-    }
-
-    public String getVersionNumber(){
-        return footerText.getText();
-    }
-}
